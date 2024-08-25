b86343b4f903fc5c8e6473c01f1be8b388eea2b9
Xu <Xxu_tao@163.com>
2024-08-25 17:04:48 +0800

    创建main.cpp文件，写了初始化代码

---
+++ main.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+
+void sort(int arr[], int size)
+{
+
+}
+
+int main()
+{
+    int arr[] = {12, 3, 89, 43, 21, 78};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    sort(arr, size);
+
+    return 0;
+}
